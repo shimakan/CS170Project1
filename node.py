4d6aa3ec924a20f5426f17ef9118f7bd444e4870
david <davidgonzalez099144@gmail.com>
2022-10-16 15:51:40 -0700

    created the node class and primary functions

--- node.py
+++ node.py
@@ -1,3 +1,46 @@
-class node:
-    def __init__(self,board,parent,move,depth,stepcost,pathcost,heuristiccost):
-        parent = parent
+from turtle import up
+
+
+class Node:
+    def __init__(self, BoardState, Parent, Depth, StepCost, PathCost, HeuristicCost):
+        BoardState = BoardState  
+        Parent = Parent
+        Depth = Depth
+        StepCost = StepCost
+        PathCost = PathCost
+        HeuristicCost = HeuristicCost
+
+        # Children Nodes
+        left  = None
+        down = None
+        up = None
+        right = None
+
+    def MoveLeft(self):
+        # returns the resulting board state after moving the blank node left        
+
+        #return child
+        pass
+
+    def MoveDown(self):
+        # returns the resulting board state after moving the blank node down
+
+        #return child
+        pass
+
+    def MoveUp(self):
+        # returns the resulting board state after moving the blank node up
+
+        #return child
+        pass
+
+    def MoveRight(self):
+        # returns the resulting board state after moving the blank node right
+       
+        #return child
+        pass
+
+    def UniformCost(self):
+        # uniform cost search
+        #GoalState = ...
+        pass
